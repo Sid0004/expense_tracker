6b3714dc2a96170a67fdc09590ad2344faef3c8d
siddhant uwu <118331299+Sid0004@users.noreply.github.com>
2024-06-29 15:32:15 +0530

    Add files via upload

---
+++ expense_trackerrr.sql
@@ -0,0 +1,16 @@
+create database expense_trackerrr;
+use expense_trackerrr;
+CREATE TABLE expenses (
+    expense_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    date DATE,
+    category VARCHAR(50),
+    description TEXT,
+    amount DECIMAL(10, 2),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
